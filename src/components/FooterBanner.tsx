98e796037a1ca71bf17d2606d2d2e1e66ed7dc81
Chetan  Mohane <107939858+Chetanmohane@users.noreply.github.com>
2026-09-14 22:59:45 +0530

    Fix missing useState import in FooterBanner

--- src/components/FooterBanner.tsx
+++ src/components/FooterBanner.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaPhoneAlt, FaEnvelope, FaGlobe } from "react-icons/fa";
 import { usePlanConfig } from "../utils/planConfig";
 
